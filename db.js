836c400e8f945a323cdf90aa1ca56c2bb973ac6b
Yusuf karback <yusufkarback64@gmail.com>
2024-06-29 11:55:24 +0700

    initial commit

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 const admin = require('firebase-admin')
-
 var serviceAccount = require("./service-account.json");
-
 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://server-monitoring-19773-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
-
-
 
 module.exports = admin
